5557affe8f89f1ab6e3ab027bb59c82ae1bc8dad
NikKo <korolyvnikita@yandex.ru>
2020-08-05 15:00:45 +0300

    Module 4.2 Values

---
+++ Values/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Тип Byte");
+        System.out.println("Минимальное значинае " + Byte.MIN_VALUE + ", максимальное " + Byte.MAX_VALUE);
+
+        System.out.println("Тип Short");
+        System.out.println("Минимальное значинае " + Short.MIN_VALUE + ", максимальное " + Short.MAX_VALUE);
+
+        System.out.println("Тип Integer");
+        System.out.println("Минимальное значинае " + Integer.MIN_VALUE + ", максимальное " + Integer.MAX_VALUE);
+
+        System.out.println("Тип Long");
+        System.out.println("Минимальное значинае " + Long.MIN_VALUE + ", максимальное " + Long.MAX_VALUE);
+
+        System.out.println("Тип Float");
+        System.out.println("Минимальное значинае " + Float.MIN_VALUE + ", максимальное " + Float.MAX_VALUE);
+
+        System.out.println("Тип Double");
+        System.out.println("Минимальное значинае " + Double.MIN_VALUE + ", максимальное " + Double.MAX_VALUE);
+
+
+
+
+
+
+
+    }
+}
