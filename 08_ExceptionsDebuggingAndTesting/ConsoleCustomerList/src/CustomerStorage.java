7f98d6871fb6965f3b19da88128e8cafbe9898d1
NikKo <korolyvnikita@yandex.ru>
2020-11-09 16:55:01 +0300

    Module 8.4 HomeWork 8.1 CustomerStorage

--- 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
+++ 08_ExceptionsDebuggingAndTesting/ConsoleCustomerList/src/CustomerStorage.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.regex.Pattern;
 
 public class CustomerStorage
 {
@@ -12,6 +13,16 @@ public class CustomerStorage
     public void addCustomer(String data)
     {
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Wrong customer format! Correct format: " +
+                    "Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
+        if(!Pattern.matches("[a-zA-z0-9.]*[a-zA-z0-9]@[a-zA-Z]+.[a-z]{2,3}",components[2])) {
+            throw new IllegalArgumentException("Wrong e-mail address! Correct format: vasily.petrov@gmail.com");
+        }
+        if(!Pattern.matches("\\+[7-8]\\d{10}",components[3])) {
+            throw new IllegalArgumentException("Wrong customer number! Correct format: +71234567890");
+        }
         String name = components[0] + " " + components[1];
         storage.put(name, new Customer(name, components[3], components[2]));
     }
